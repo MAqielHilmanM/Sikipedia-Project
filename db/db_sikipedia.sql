70b8d2ed2b00254845d84f41a5ef93f220543b91
maqielhm <maqiel2600@gmail.com>
2019-04-23 23:39:01 +0700

    fix database

--- db/db_sikipedia.sql
+++ db/db_sikipedia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 03, 2019 at 06:07 AM
+-- Generation Time: Apr 23, 2019 at 06:38 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -32,10 +32,18 @@ USE `db_sikipedia`;
 
 DROP TABLE IF EXISTS `t_article`;
 CREATE TABLE `t_article` (
-  `id` int(11) NOT NULL,
+  `id` varchar(40) NOT NULL,
   `article` text NOT NULL,
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_article`
+--
+
+INSERT INTO `t_article` (`id`, `article`, `create_date`) VALUES
+('Loreas_Lore', '\'\'\' Lorem ipsum \'\'\' dolor sit amet, et est natum salutandi, eum minimum dignissim te, assentior democritum vim in. Eum no aperiri assueverit, at pri harum saperet adipisci, tota expetenda ne usu. Eu adhuc dicunt sit. Ei saepe nominavi facilisis mea, in sit deleniti appetere tincidunt, sed an erat vivendo.\r\n\r\n== Lorem Sample 1 ==\r\nNam ad ipsum mazim quaestio. At vide iriure repudiare per, ei prima mundi electram per. An sea scriptorem referrentur. Ne vim brute porro aliquando. Dico dolorem ad sit. Cum malis saepe appetere in, deleniti eloquentiam ullamcorper in sea.\r\n\r\n== Lorem Sample 2 ==\r\nAn aliquam deserunt vis, qui in veri populo. Vis ex modo duis graeci. Ut tincidunt voluptatum vim, malis propriae accusata ne cum, tamquam verterem ad pro. Ius te copiosae luptatum convenire, ad alia admodum his, feugait urbanitas eu has. Vel graeco mandamus eu, ne sed labore officiis mnesarchum, mutat virtute nam et. Vis adipisci instructior ea, his ut aperiam consetetur sadipscing, ei mel sale delectus.\r\n\r\n== Lorem Sample 3 ==\r\nUsu impetus petentium in, ei probo quaeque iracundia eos, vix homero repudiare id. Fastidii mediocrem et vis, augue tincidunt consequuntur eam eu, vix primis detracto cu. Justo scribentur sit ne. Phaedrum gloriatur ut duo. Inermis graecis petentium eos eu.\r\n\r\n== Lorem Sample 4 ==\r\nIisque oblique bonorum cum an, eam no nonumy instructior. Et vix duis accusam ocurreret. Eam graeco ancillae expetendis ea, has verear iudicabit eu, ius ullum elitr omnes an. Per eu euismod perfecto. In cum amet habeo illum, quod audire iudicabit quo at.', '2019-04-23 15:54:03'),
+('lorem_ipsum', 'lorem ipsum sim dolor amet\r\n\r\n== For Your Information ==\r\nlorem ipsum sim dolor amet is \'\'lorem\'\' ipsum sim \'\'\'dolor\'\'\'\r\n\r\n=== next information ===\r\nso the next information is. . . \r\n\r\n\r\n\'\' hidden \'\'', '2019-04-23 13:46:39');
 
 -- --------------------------------------------------------
 
@@ -46,10 +54,18 @@ CREATE TABLE `t_article` (
 DROP TABLE IF EXISTS `t_contributor`;
 CREATE TABLE `t_contributor` (
   `id` int(11) NOT NULL,
-  `id_article` int(11) NOT NULL,
+  `id_article` varchar(40) NOT NULL,
   `id_user` int(11) NOT NULL,
+  `summary` varchar(255) NOT NULL,
   `edit_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_contributor`
+--
+
+INSERT INTO `t_contributor` (`id`, `id_article`, `id_user`, `summary`, `edit_date`) VALUES
+(1, 'lorem_ipsum', 1, 'sample text', '2019-04-23 11:19:44');
 
 -- --------------------------------------------------------
 
@@ -60,11 +76,18 @@ CREATE TABLE `t_contributor` (
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
   `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_user`
+--
+
+INSERT INTO `t_user` (`id`, `email`, `username`, `password`) VALUES
+(1, 'maqiel2600@gmail.com', 'maqielhm', 'aaaaaaaaaaaaaaaa'),
+(3, 'aqiel', 'aqiel', '5ad62906c0a165807687018c82b36db8');
 
 --
 -- Indexes for dumped tables
@@ -95,22 +118,16 @@ ALTER TABLE `t_user`
 --
 
 --
--- AUTO_INCREMENT for table `t_article`
---
-ALTER TABLE `t_article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `t_contributor`
 --
 ALTER TABLE `t_contributor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `t_user`
 --
 ALTER TABLE `t_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
